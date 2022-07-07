9e09320999a94883b81937e2b614c2343c326680
PolinaKrasovskaya <polina.vorotnik@gmail.com>
2022-07-07 16:35:31 +0100

    done

---
+++ controllers/contacts/updateById.js
@@ -0,0 +1,18 @@
+const contacts = require("../../models/contacts");
+const { createError } = require("../../helpers");
+const { addSchema } = require("../../schemas/contacts");
+
+const updateById = async (req, res, next) => {
+    const {error} = addSchema.validate(req.body);
+    if(error){
+        throw createError(400, error.message);
+    }
+    const {contactId} = req.params;
+    const result = await contacts.updateContact(contactId, req.body);
+    if(!result){
+        throw createError(404);
+    }
+    res.json(result);
+};
+
+module.exports = updateById;
