8bfc9a1e6a3bd1387a4e79ea0e47427c7e48f7ca
PolinaKrasovskaya <72392978+PolinaKrasovskaya@users.noreply.github.com>
2022-07-22 05:10:19 +0300

    Update users.test.js

--- routes/api/users.test.js
+++ routes/api/users.test.js
@@ -17,9 +17,7 @@ describe("test user routes", () => {
     });
 
     afterEach((done)=> {
-        // mongoose.connection.db.dropCollection(() => {
             mongoose.connection.close(() => done())
-        // })
     });
 
     test("test login route", async() => {
